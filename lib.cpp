816afd3b57aa7e3c8f6bc1f7dd8cb3765478421f
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-12-17 05:34:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,12 @@
 #include "lib.h"
-bool verifica(char listone[10][20], char name[20], int &pos){
+bool controllo(char nomi[10][20], char nome[20], int &c){
     for (int n = 0; n < 10; n++){
-        for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 20; i++)
         {
-            if (listone [n][i] != name[i]){
+            if (nomi [n][i] != nome[i]){
                 i = 20;  }
             if (i == 19){
-                pos = n;
+                c = n;
                 return true;}
         }
     }
