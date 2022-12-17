6de36292ff4c609c34e0565b112c96661db890ba
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-12-17 05:28:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
-}
+
+    char listone[10][20];
+    char name[20];
+    int pos = 0;
+
+    for (int i = 0; i < 10; i++){
+        
+        for (int j = 0; j < 20; j++){
+             
+             name[j] = ' ';
+            listone[i][j] = ' ';
+        }
+    }
+
+    for (int i = 0; i < 10; i++){
+    
+        cin >> listone[i];
+    }
+    cin >> name;
+
+    if (verifica(listone , name, pos)){
+       
+       cout << pos<< endl;
+    }
+    else{
+    
+        cout << "non presente"<<endl;
+    }
+
+    return 0;
+]
